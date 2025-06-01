8bf4388290e4383fef599759bfbca2d4263b63f5
Nipun Wijethunga <nmwijethunga@students.nsbm.ac.lk>
2025-06-02 00:04:45 +0530

    Removed unused file.

--- SQLQuery2.sql
+++
@@ -1,9 +0,0 @@
-﻿-- Step 1: Delete all existing data
-DELETE FROM RoleButtons;
-
--- Step 2: Reset identity seed to 0 (so next insert starts at 1)
-DBCC CHECKIDENT ('RoleButtons', RESEED, 0);
-
--- Step 3: Insert the new record
-INSERT INTO RoleButtons (RoleID, ButtonName, LinkedFormName)
-VALUES (1, 'Menu', 'MenuForm');
